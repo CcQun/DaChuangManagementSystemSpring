2ad4de252e415a1948003345195a4cf065578608
Shadownow <48984034+Shadownow@users.noreply.github.com>
2020-06-04 01:05:13 +0800

    文件上传

--- src/main/java/com/example/demo/controller/ElseController.java
+++ src/main/java/com/example/demo/controller/ElseController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.demo.core.Utils;
 import com.example.demo.core.request.LoginRequest;
 import com.example.demo.core.response.BaseResponse;
@@ -7,10 +8,11 @@ import com.example.demo.core.response.DataResponse;
 import com.example.demo.db.model.Student;
 import com.example.demo.db.service.ApplyBlinkService;
 import com.example.demo.db.service.StudentService;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 
@@ -26,6 +28,49 @@ public class ElseController {
     public ElseController(StudentService studentService) {
         this.studentService = studentService;
     }
+
+
+    //上传图片
+    @ResponseBody
+    @RequestMapping(value = "/fileUpload")
+    public JSONObject fileUpload(@RequestParam("file") MultipartFile file,@RequestParam("name") String name){
+        if(file.isEmpty()){
+            JSONObject json = new JSONObject();
+            json.put("response", "false");
+            return json;
+        }
+        //String fileName = file.getOriginalFilename();
+        String fileName = name;
+        System.out.println(fileName);
+        int size = (int) file.getSize();
+        System.out.println(fileName + "-->" + size);
+
+        String path = "D:\\Aa大学文件\\处理事项\\大三下\\课程-JavaEE\\DaChuangManagementSystemSpring\\src\\main\\resources\\static";
+        File dest = new File(path + "/" + fileName);
+        if(!dest.getParentFile().exists()){ //判断文件父目录是否存在
+            dest.getParentFile().mkdir();
+        }
+        try {
+            file.transferTo(dest); //保存文件
+            JSONObject json = new JSONObject();
+            json.put("response", "true");
+            json.put("fileindex", fileName);
+            return json;
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            JSONObject json = new JSONObject();
+            json.put("response", "false");
+            return json;
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            JSONObject json = new JSONObject();
+            json.put("response", "false");
+            return json;
+        }
+    }
+
 
     @RequestMapping("/login")
     public BaseResponse login(@RequestBody LoginRequest request){
