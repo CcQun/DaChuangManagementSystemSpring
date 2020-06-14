d5f18282d80911f00669eeb9e32c79c7ec536f31
Shadow1300 <871744494@qq.com>
2020-06-14 10:59:43 +0800

    从blink转到Project的，添加到team表中

--- src/main/java/com/example/demo/db/service/AbstractService.java
+++ src/main/java/com/example/demo/db/service/AbstractService.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import com.example.demo.db.model.ApplyBlinkPK;
 
 import java.util.List;
 
