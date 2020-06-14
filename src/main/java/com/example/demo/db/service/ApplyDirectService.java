d4a933a7737ca851ab0679986eca4a53e2b1a3ab
Shadownow <48984034+Shadownow@users.noreply.github.com>
2020-06-14 11:35:35 +0800

    老师查看自己的指导老师申请

--- src/main/java/com/example/demo/db/service/ApplyDirectService.java
+++ src/main/java/com/example/demo/db/service/ApplyDirectService.java
@@ -1,9 +1,14 @@
 package com.example.demo.db.service;
 
 import com.example.demo.db.mapper.ApplyDirectMapper;
+import com.example.demo.db.model.ApplyBlink;
 import com.example.demo.db.model.ApplyDirect;
 import com.example.demo.db.model.ApplyDirectPK;
+import com.example.demo.db.model.Student;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @Author CcQun
@@ -11,4 +16,33 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ApplyDirectService extends BaseService<ApplyDirect, ApplyDirectPK, ApplyDirectMapper> {
+    public boolean insert(ApplyDirect applyDirect) {
+        try {
+            mapper.save(applyDirect);
+            return true;
+        } catch (Exception e) {
+            System.out.println("主键冲突");
+            return false;
+        }
+    }
+
+    public boolean findAllByDirectProject(ApplyDirect applyDirect, int approval) {
+
+        applyDirect.setDirect_Approval(approval);
+
+        try {
+
+            mapper.save(applyDirect);
+
+            return true;
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+            return false;
+
+        }
+
+    }
 }
